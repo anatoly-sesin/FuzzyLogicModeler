d532d95c3ac09a8ed38c85e28fa351231cbbe3c1
anatoly-sesin <anatolysesin@ya.ru>
2024-10-10 13:33:52 +0300

    add sugeno

--- main.py
+++ main.py
@@ -13,37 +13,7 @@ st.set_page_config(page_title="Fuzzy Logic Toolbox", layout="wide")
 def initialize_variables():
     if 'linguistic_variables' not in st.session_state:
         st.session_state.linguistic_variables = []
-        # st.session_state.linguistic_variables = [
-        #     LinguisticVariable("soil moisture", 0, 50, "input"),
-        #     LinguisticVariable("relative humidity", 0, 100, "input"),
-        #     LinguisticVariable("air temperature", 0, 40, "input"),
-        #     LinguisticVariable("water pump pressure", 0, 100, "output")
-        # ]
         
-        # Add terms to each linguistic variable
-        # soil_moisture = st.session_state.linguistic_variables[0]
-        # soil_moisture.add_term("saturated", "trapezoidal", [30, 40, 50, 50])
-        # soil_moisture.add_term("optimal", "triangular", [15, 25, 35])
-        # soil_moisture.add_term("insufficient", "trapezoidal", [0, 0, 10, 20])
-
-        # relative_humidity = st.session_state.linguistic_variables[1]
-        # relative_humidity.add_term("low", "trapezoidal", [0, 0, 20, 40])
-        # relative_humidity.add_term("medium", "triangular", [30, 50, 70])
-        # relative_humidity.add_term("normal", "triangular", [60, 75, 90])
-        # relative_humidity.add_term("high", "trapezoidal", [80, 90, 100, 100])
-
-        # air_temperature = st.session_state.linguistic_variables[2]
-        # air_temperature.add_term("low", "trapezoidal", [0, 0, 10, 15])
-        # air_temperature.add_term("medium", "triangular", [10, 20, 30])
-        # air_temperature.add_term("normal", "triangular", [25, 30, 35])
-        # air_temperature.add_term("high", "trapezoidal", [30, 35, 40, 40])
-
-        # water_pump_pressure = st.session_state.linguistic_variables[3]
-        # water_pump_pressure.add_term("very weak", "trapezoidal", [0, 0, 10, 20])
-        # water_pump_pressure.add_term("weak", "triangular", [10, 25, 40])
-        # water_pump_pressure.add_term("medium", "triangular", [30, 50, 70])
-        # water_pump_pressure.add_term("high", "triangular", [60, 75, 90])
-        # water_pump_pressure.add_term("maximum", "trapezoidal", [80, 90, 100, 100])
     
     if 'fuzzy_rules' not in st.session_state:
         st.session_state.fuzzy_rules = []
